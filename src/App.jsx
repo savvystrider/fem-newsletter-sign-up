bd4502913d45f5a905fb538d5b8b265e12f149c5
Manish <mjpatel84@gmail.com>
2023-12-10 12:52:24 -0500

    adds css variables and basic body styles

--- src/App.jsx
+++ src/App.jsx
@@ -3,14 +3,23 @@ import Form from "./components/Form";
 export default function App() {
   return (
     <div className="container">
-      <h1>Stay updated!</h1>
-      <p>Join 60,000+ product managers receiving monthly updates on:</p>
-      <ul>
-        <li>Product discovery and building what matters</li>
-        <li>Measuring to ensure updates are a success</li>
-        <li>And much more!</li>
-      </ul>
-      <Form />
+      <main>
+        <h1>Stay updated!</h1>
+        <p>Join 60,000+ product managers receiving monthly updates on:</p>
+        <ul>
+          <li>Product discovery and building what matters</li>
+          <li>Measuring to ensure updates are a success</li>
+          <li>And much more!</li>
+        </ul>
+        <Form />
+      </main>
+      <div className="img-container">
+        <img
+          src="assets/illustration-sign-up-mobile.svg"
+          alt=""
+          className="hero-img"
+        />
+      </div>
     </div>
   );
 }
