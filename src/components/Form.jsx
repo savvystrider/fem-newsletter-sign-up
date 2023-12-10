2cdb503e096c7a31b6630e960a7c5e5e338e2a74
Manish <mjpatel84@gmail.com>
2023-12-10 13:37:17 -0500

    adds error handling and styles

--- src/components/Form.jsx
+++ src/components/Form.jsx
@@ -1,17 +1,40 @@
+import { useState } from "react";
+
 export default function Form() {
+  const [emailInput, setEmailInput] = useState("");
+  const [isValidEmail, setIsValidEmail] = useState(true);
+
+  function handleChange(e) {
+    const { value } = e.target;
+    setEmailInput(value);
+    const validEmail = /\S+@\S+\.\S+/.test(value);
+    setIsValidEmail(validEmail);
+  }
+
   return (
     <form>
       <div className="input-container">
+        <div className="input-row">
+          <label htmlFor="email">Email address</label>
+          {!isValidEmail && (
+            <span className="error-message">Valid email required</span>
+          )}
+        </div>
         <input
           type="email"
-          className="email-input"
+          className={`email-input ${!isValidEmail ? "error-input" : ""}`}
           id="email"
           placeholder="email@company.com"
+          value={emailInput}
+          onChange={handleChange}
         />
-        <label htmlFor="email">Email address</label>
       </div>
-      <button className="btn subscribe-btn">
-        Subscribe to montly newsletter
+      <button
+        className="btn subscribe-btn"
+        type="submit"
+        onClick={(e) => e.preventDefault()}
+      >
+        Subscribe to monthly newsletter
       </button>
     </form>
   );
